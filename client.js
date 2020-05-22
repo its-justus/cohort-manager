c6d6c9c475302aeedca2a114c4d18cd14d60a5d7
its-justus <ianjohnson9042@gmail.com>
2020-05-22 11:15:16 -0500

    table loads items, added clear table button

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-let cohort = [
+let cohorts = [
     {
         name: 'Alpha',
         status: 'Tier 3',
@@ -25,8 +25,27 @@ function clearTable(){
     $('table tbody').empty();
 }
 
-function setup() {
+function refreshTable(){
     clearTable();
+    let tBody = $('table tbody');
+    for(let cohort of cohorts){
+        let tRow = $('<tr></tr>');
+        for(let key in cohort){
+            let tCell = $(`<td>${cohort[key]}</td>`);
+            tRow.append(tCell);
+        }
+        tRow.append($('<button>Delete</button>'));
+        tBody.append(tRow);
+    }
+}
+
+function setup() {
+    // set up DOM
+    refreshTable();
+    $('#buttonClearTable').on('click', function(){
+        $('table tbody').empty();
+    });
+    
 }
 
 $('document').ready(setup)
